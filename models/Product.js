b415adac41b66ced08a949f94e550ed42114aeb1
Jbolts <77908382+Jbolton89@users.noreply.github.com>
2021-06-04 16:53:10 +1000

    models

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,31 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: { 
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      autoIncrement: true, 
+      primaryKey: true, 
+    },
+    product_name: { 
+      type: DataTypes.STRING, 
+      allowNull: false
+    },
+    price: { 
+      type: DataTypes.DECIMAL(10, 2), 
+      allowNull: false, 
+      validate: { isDecimal: true }
+    }, 
+    stock: { 
+      type:DataTypes.INTEGER,
+      allowNull: false, 
+      defaultValues: 10,
+      validate: { isNumeric: true }
+    },
+    category_id: { 
+      type: DataTypes.INT, 
+      references: { model: 'Category', key: 'id' }
+    }
   },
   {
     sequelize,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,4 +1,4 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
@@ -7,6 +7,20 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: { 
+      type: DataTypes.INT, 
+      allowNull: false, 
+      primaryKey: true, 
+      autoIncrement: true
+    }, 
+    product_id: {
+      type: DataTypes.INT, 
+      references: { model: 'Product', key: 'id' }
+    },
+    tag_id: { 
+      type: DataTypes.INT, 
+      references: { model: 'Tag', key: 'id' }
+    },
   },
   {
     sequelize,
